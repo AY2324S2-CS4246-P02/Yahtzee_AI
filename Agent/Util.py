d7c571870555529f4220e1d954ca6e802530a3f9
harin0826 <e0411191@u.nus.edu>
2024-03-07 18:01:54 +0800

    Add potentially useful function to Util.py

--- Agent/Util.py
+++ Agent/Util.py
@@ -1,2 +1,14 @@
 import numpy as np
 
+def get_roll_prob(given, indices, goal):
+    """
+    Returns probability of rerolling certain combination of dice given current dice.
+
+    Parameter:
+    given (array of int): numpy array of current dice
+    indices (array of int): indices of dices to reroll
+    goal (dict of int): counts of die faces
+    """
+    pass
+
+
