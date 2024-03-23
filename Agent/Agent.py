6b576c7d2daa98009e9026f890a79726992ea11b
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2024-03-23 19:35:44 +0800

    Update Base Agent and PlayByHandAgent.
    Base Agent: Include Yahtzee in base agent.
    Base Agent: Include play_game method.
    PlayByHandAgent: display dice on each roll.

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import List, Literal, Union, Tuple
+from Yahtzee import CATEGORIES_NAMES
+import Yahtzee
 
 class Agent(ABC):
+
+    def __init__(self):
+        self.game = Yahtzee.Yahtzee()
+        self.move_history = []
+
+    def __category_idx_to_str(
+            categories: List[int]
+        ) -> List[str]:
+        return list(map(lambda idx: CATEGORIES_NAMES[idx], categories))
 
     @abstractmethod
     def get_agent_name(
@@ -17,3 +28,37 @@ class Agent(ABC):
         available_categories: List[str]
     ) -> Tuple[Literal['REROLL', 'KEEP'], Union[List[bool], str]]:
         raise NotImplementedError
+    
+    def play_game(self):
+        for round in range(13):
+            chosen_category = None
+            while self.game.get_rerolls() > 0:
+                action = self.get_action(
+                    self.game.get_dice(),
+                    self.game.get_rerolls(),
+                    Agent.__category_idx_to_str(self.game.get_available_categories())
+                )
+
+                self.move_history.append((round, action))
+
+                if action[0] == 'REROLL':
+                    self.game.roll_dice(action[1])
+                else:
+                    chosen_category = action[1]
+                    break
+            if chosen_category == None:
+                action = self.get_action(
+                    self.game.get_dice(),
+                    self.game.get_rerolls(),
+                    Agent.__category_idx_to_str(self.game.get_available_categories())
+                )
+
+                self.move_history.append((round, action))
+
+                chosen_category = action[1]
+
+            self.game.write_score(chosen_category)
+
+        print("\n".join(list(map(
+            lambda entry: f"Round {entry[0]}: {entry[1][0]} -> {entry[1][1]}"
+        ))))
